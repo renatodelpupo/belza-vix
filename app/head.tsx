5a77b46c3142ddd925efa7ad78a317186f432146
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2023-06-07 18:07:05 -0300

    feat: use svg favicon

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Belza Vix</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <meta name='description' content='Belza Vix Website' />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='/favicon.svg' />
     </>
   );
 }
